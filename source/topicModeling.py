97cb7c8edd2eeec18dd6c04bdc45fe1d1ecca9f9
qi97 <77075274+Qi9726@users.noreply.github.com>
2023-12-14 22:16:22 -0500

    Update topicModeling.py

--- source/topicModeling.py
+++ source/topicModeling.py
@@ -1,11 +1,14 @@
+# -*- coding: utf-8 -*-
+
 import pandas as pd
+
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import RegexpTokenizer
+
 import gensim.corpora as corpora
 from gensim.models import LdaModel, TfidfModel
 
-#nltk.download('punkt')
 nltk.download('stopwords')
 
 data_path ='IF_output.csv'
@@ -13,37 +16,34 @@ retrival_output = pd.read_csv(data_path)
 
 text = retrival_output["text"].astype(str).str.replace(r'#\w+', '', regex=True)
 
-# RegEx for words with length > 3
+# Tokenize the text using a regular expression to retain words with a length of at least three characters
 tokenizer = RegexpTokenizer(r'\b[a-zA-Z]{3,}\b')
 tokenized_text = [tokenizer.tokenize(s) for s in text]
 
-# remove stop words
+# Remove stopwords from the tokenized text
 stop_words = stopwords.words('english')
-tokenized_des = [[w for w in s if w not in stop_words] for s in tokenized_text]
+tokenized_all = [list(filter(lambda w: w not in stop_words, s)) for s in tokenized_text]
 
+# Create a Gensim dictionary and a bag-of-words representation (corpus) from the tokenized text
+document_all = corpora.Dictionary(tokenized_all)
+corpus = [document_all.doc2bow(t) for t in tokenized_all]
 
-# create corpus
-dct_des = corpora.Dictionary(tokenized_des)
-corpus_des = [dct_des.doc2bow(line) for line in tokenized_des]
+# Create a TF-IDF model based on the corpus
+tfidf_model = TfidfModel(corpus)
 
+# Identify low-value words (words with low TF-IDF scores) based on a threshold (0.1)
+words_lowValue = [id for bow in corpus for id, value in tfidf_model[bow] if value < 0.1]
 
-tfidf_model = TfidfModel(corpus_des)  # fit tf-idf model
+# Filter out low-value words from the dictionary and the corpus
+document_all.filter_tokens(bad_ids=words_lowValue)
 
-low_value_words = []
-for bow in corpus_des:
-    low_value_words += [id for id, value in tfidf_model[bow] if value < 0.1]
-
-#[dct_des.get(low_value_words[i]) for i in range(10)]
-
-# filter words from the dictionary and the corpus
-dct_des.filter_tokens(bad_ids=low_value_words)
-corpus_des = [dct_des.doc2bow(line) for line in tokenized_des]
-
-# topics_num = 1
-
+corpus = []
+for line in tokenized_all:
+    bow_representation = document_all.doc2bow(line)
+    corpus.append(bow_representation)
 def format_lda_topics(topics_num):
-    lda = LdaModel(corpus_des, num_topics=topics_num, id2word=dct_des, passes=10, iterations=500)
-    # Extract topics
+    lda = LdaModel(corpus, num_topics=topics_num, id2word=document_all, passes=10, iterations=100)
+
     topics = lda.show_topics()
 
     # Convert topics to a list of dictionaries
@@ -56,4 +56,4 @@ def format_lda_topics(topics_num):
     return formatted_topics
 
 formatted_topics = format_lda_topics(3)
-# print(formatted_topics)
+print(formatted_topics)
